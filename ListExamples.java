f7398c7f4b5785c467f35e08b7dcabc01adb2c67
ryanmartinez1 <ryantmartinez23@gmail.com>
2022-12-05 20:25:32 -0800

    fixed

--- ListExamples.java
+++ ListExamples.java
@@ -8,11 +8,11 @@ class ListExamples {
   // Returns a new list that has all the elements of the input list for which
   // the StringChecker returns true, and not the elements that return false, in
   // the same order they appeared in the input list;
-  static List<String> filter(StringChecker sc, List<String> list) {
+  static List<String> filter(List<String> list, StringChecker sc) {
     List<String> result = new ArrayList<>();
     for(String s: list) {
       if(sc.checkString(s)) {
-        result.add(s);
+        result.add(0,s);
       }
     }
     return result;
